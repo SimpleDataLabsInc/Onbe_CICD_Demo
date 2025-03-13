b2d8d5a5fbfb649753805dd39473ec6832a4470d
best.neil@prophecy.io <best.neil@prophecy.io>
2025-03-13 14:28:19 +0000

    Disable both jobs; refresh project metadata and DAG IDs

--- jobs/run_all/code/dag.py
+++ jobs/run_all/code/dag.py
@@ -7,11 +7,11 @@ from airflow import DAG
 from airflow.models.param import Param
 from airflow.decorators import task
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-from y6vj_vjzn15xaacjbch0kq_.tasks import dev
+from _0qytbn7imajrwaeacbx9uq_.tasks import dev
 PROPHECY_RELEASE_TAG = "__PROJECT_ID_PLACEHOLDER__/__PROJECT_RELEASE_VERSION_PLACEHOLDER__"
 
 with DAG(
-    dag_id = "Y6vJ_vJzN15XaACjbCh0KQ_", 
+    dag_id = "0qyTBN7imaJRWaeACBX9UQ_", 
     schedule_interval = "0/1 * * * *", 
     default_args = {"owner" : "Prophecy", "retries" : 0, "ignore_first_depends_on_past" : True, "do_xcom_push" : True, "pool" : "MIio4Toq"}, 
     params = {'env_dev' : Param("""DEV""", type = "string", title = """env_dev""")}, 
